jdelgadoalfonso/sdcc

commit 4e8be7df15c399a7ce79aeeeb0062ab1b62d5f63

* configure.in, debugger/mcs51/configure.in,   device/lib/pic/configure.in, device/lib/pic16/configure.ac,   sim/ucsim/configure.in, sim/ucsim/custom.h,   sim/ucsim/ddconfig_in.h, support/packihx/configure.in:   updated build system to allow autoreconf -fi to set up the project
git-svn-id: svn://svn.code.sf.net/p/sdcc/code@5954 4a8a32a2-be11-0410-ad9d-d568d2c75423

trunk/sdcc/configure.in
@@ -27,10 +27,10 @@ AC_SUBST(VERSION)
 AC_SUBST(VERSIONHI)
 AC_SUBST(VERSIONLO)
 AC_SUBST(VERSIONP)
-AC_DEFINE_UNQUOTED(SDCC_VERSION_LO, ${VERSIONLO})
-AC_DEFINE_UNQUOTED(SDCC_VERSION_HI, ${VERSIONHI})
-AC_DEFINE_UNQUOTED(SDCC_VERSION_P, ${VERSIONP})
-AC_DEFINE_UNQUOTED(SDCC_VERSION_STR, "${VERSION}")
+AC_DEFINE_UNQUOTED(SDCC_VERSION_LO, ${VERSIONLO}, [XXX])
+AC_DEFINE_UNQUOTED(SDCC_VERSION_HI, ${VERSIONHI}, [XXX])
+AC_DEFINE_UNQUOTED(SDCC_VERSION_P, ${VERSIONP}, [XXX])
+AC_DEFINE_UNQUOTED(SDCC_VERSION_STR, "${VERSION}", [XXX])
 AC_ARG_PROGRAM
 sdcc_cv_version=$VERSION
 sdcc_cv_versionhi=$VERSIONHI
@@ -115,7 +115,7 @@ rm conftest.err conftest.out
 
 # Check whether the compiler for the current language is SunPRO.
 
-AC_DEFUN(bor_IS_SUNPRO,
+AC_DEFUN([bor_IS_SUNPRO],
 [AC_CACHE_CHECK([whether we are using the SunPRO _AC_LANG compiler],
   [bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro],
   [_AC_COMPILE_IFELSE([AC_LANG_PROGRAM([], [[#if !defined __SUNPRO_C && !defined __SUNPRO_CC
@@ -132,7 +132,7 @@ bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro=$bor_compiler_sunpro
 # the compiler returns an error or in case something is writeen to stderr.
 # For example: bor_VALID_OPT(fPIC)
 
-AC_DEFUN(bor_IS_VALID_OPTION,
+AC_DEFUN([bor_IS_VALID_OPTION],
 [ac_test_[]_AC_LANG_PREFIX[]FLAGS=${[]_AC_LANG_PREFIX[]FLAGS+set}
 bor_save_[]_AC_LANG_PREFIX[]FLAGS=$[]_AC_LANG_PREFIX[]FLAGS
 AC_CACHE_CHECK(whether _AC_LANG accepts -$1, bor_cv_[]_AC_LANG_ABBREV[]_$1,
@@ -341,7 +341,7 @@ dnl adl_NORMALIZE_DEFINE_UNQUOTED(var, DEFINE, REFERENCE_STRING)
 AC_DEFUN([adl_NORMALIZE_DEFINE_UNQUOTED], [
   ac_ndu=[$]$1
   adl_NORMALIZE_PATH([ac_ndu], [$]$3)
-  AC_DEFINE_UNQUOTED($2, "${ac_ndu}")
+  AC_DEFINE_UNQUOTED($2, "${ac_ndu}", [XXX])
 ])
 
 dnl adl_NORMALIZE_PATH_MSG(input_string, var, dir_separator)
@@ -456,13 +456,13 @@ AC_MSG_CHECKING(type name for dword)
 TYPE_DWORD=`type_name 4`
 AC_MSG_RESULT($TYPE_DWORD)
 
-AC_DEFINE_UNQUOTED(TYPE_BYTE,  $TYPE_BYTE)
-AC_DEFINE_UNQUOTED(TYPE_WORD,  $TYPE_WORD)
-AC_DEFINE_UNQUOTED(TYPE_DWORD, $TYPE_DWORD)
+AC_DEFINE_UNQUOTED(TYPE_BYTE,  $TYPE_BYTE,  [XXX])
+AC_DEFINE_UNQUOTED(TYPE_WORD,  $TYPE_WORD,  [XXX])
+AC_DEFINE_UNQUOTED(TYPE_DWORD, $TYPE_DWORD, [XXX])
 
-AC_DEFINE_UNQUOTED(TYPE_UBYTE,  unsigned $TYPE_CHAR)
-AC_DEFINE_UNQUOTED(TYPE_UWORD,  unsigned $TYPE_WORD)
-AC_DEFINE_UNQUOTED(TYPE_UDWORD, unsigned $TYPE_DWORD)
+AC_DEFINE_UNQUOTED(TYPE_UBYTE,  unsigned $TYPE_CHAR,  [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UWORD,  unsigned $TYPE_WORD,  [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UDWORD, unsigned $TYPE_DWORD, [XXX])
 
 
 # SDCC_BUILD_BIGENDIAN ([ACTION-IF-TRUE], [ACTION-IF-FALSE], [ACTION-IF-UNKNOWN])
@@ -593,8 +593,8 @@ AC_SUBST(EXEEXT)
 # sdccconf.h
 ############
 
-AC_DEFINE_UNQUOTED(DIR_SEPARATOR_STRING, "${sdccconf_h_dir_separator}")
-AC_DEFINE_UNQUOTED(DIR_SEPARATOR_CHAR, '${sdccconf_h_dir_separator}')
+AC_DEFINE_UNQUOTED(DIR_SEPARATOR_STRING, "${sdccconf_h_dir_separator}", [XXX])
+AC_DEFINE_UNQUOTED(DIR_SEPARATOR_CHAR, '${sdccconf_h_dir_separator}', [XXX])
 
 # prefix:
 # default: "NONE", ${ac_default_prefix}: "/usr/local"
@@ -620,51 +620,51 @@ adl_NORMALIZE_DEFINE_UNQUOTED(expanded_datadir, DATADIR, sdccconf_h_dir_separato
 norm_inc_dir_suffix=${include_dir_suffix}
 adl_NORMALIZE_PATH([norm_inc_dir_suffix], [$sdccconf_h_dir_separator])
 AC_DEFINE_UNQUOTED(INCLUDE_DIR_SUFFIX,
-                   DIR_SEPARATOR_STRING "${norm_inc_dir_suffix}")
+                   DIR_SEPARATOR_STRING "${norm_inc_dir_suffix}", [XXX])
 norm_lib_dir_suffix=${lib_dir_suffix}
 adl_NORMALIZE_PATH([norm_lib_dir_suffix], [$sdccconf_h_dir_separator])
 AC_DEFINE_UNQUOTED(LIB_DIR_SUFFIX,
-                   DIR_SEPARATOR_STRING "${norm_lib_dir_suffix}")
+                   DIR_SEPARATOR_STRING "${norm_lib_dir_suffix}", [XXX])
 
 # relative paths
 adl_COMPUTE_RELATIVE_PATHS([expanded_bindir:expanded_datadir:bin2data_dir])
 adl_NORMALIZE_PATH(bin2data_dir, [$sdccconf_h_dir_separator])
 AC_DEFINE_UNQUOTED(BIN2DATA_DIR,
-                   DIR_SEPARATOR_STRING "${bin2data_dir}")
+                   DIR_SEPARATOR_STRING "${bin2data_dir}", [XXX])
 
 adl_COMPUTE_RELATIVE_PATHS([expanded_prefix:expanded_bindir:prefix2bin_dir])
 adl_NORMALIZE_PATH(prefix2bin_dir, [$sdccconf_h_dir_separator])
 AC_DEFINE_UNQUOTED(PREFIX2BIN_DIR,
-                   DIR_SEPARATOR_STRING "${prefix2bin_dir}")
+                   DIR_SEPARATOR_STRING "${prefix2bin_dir}", [XXX])
 
 adl_COMPUTE_RELATIVE_PATHS([expanded_prefix:expanded_datadir:prefix2data_dir])
 adl_NORMALIZE_PATH(prefix2data_dir, [$sdccconf_h_dir_separator])
 if test "${prefix2data_dir}" = "."; then
     # small optimization for Mingw32; otherwise Borut will complain ;-)
-    AC_DEFINE_UNQUOTED(PREFIX2DATA_DIR, "")
+    AC_DEFINE_UNQUOTED(PREFIX2DATA_DIR, "", [XXX])
 else
     AC_DEFINE_UNQUOTED(PREFIX2DATA_DIR,
-                       DIR_SEPARATOR_STRING "${prefix2data_dir}")
+                       DIR_SEPARATOR_STRING "${prefix2data_dir}", [XXX])
 fi
 
 # standard libs
-AC_DEFINE_UNQUOTED(STD_LIB,       "libsdcc")
-AC_DEFINE_UNQUOTED(STD_INT_LIB,   "libint")
-AC_DEFINE_UNQUOTED(STD_LONG_LIB,  "liblong")
-AC_DEFINE_UNQUOTED(STD_FP_LIB,    "libfloat")
-AC_DEFINE_UNQUOTED(STD_DS390_LIB, "libds390")
-AC_DEFINE_UNQUOTED(STD_DS400_LIB, "libds400")
-AC_DEFINE_UNQUOTED(STD_XA51_LIB,  "libxa51")
+AC_DEFINE_UNQUOTED(STD_LIB,       "libsdcc", [XXX])
+AC_DEFINE_UNQUOTED(STD_INT_LIB,   "libint", [XXX])
+AC_DEFINE_UNQUOTED(STD_LONG_LIB,  "liblong", [XXX])
+AC_DEFINE_UNQUOTED(STD_FP_LIB,    "libfloat", [XXX])
+AC_DEFINE_UNQUOTED(STD_DS390_LIB, "libds390", [XXX])
+AC_DEFINE_UNQUOTED(STD_DS400_LIB, "libds400", [XXX])
+AC_DEFINE_UNQUOTED(STD_XA51_LIB,  "libxa51", [XXX])
 
 # SDCC runtime environment variables
 sdcc_dir_name="SDCC_HOME"
-AC_DEFINE_UNQUOTED(SDCC_DIR_NAME, "${sdcc_dir_name}")
+AC_DEFINE_UNQUOTED(SDCC_DIR_NAME, "${sdcc_dir_name}", [XXX])
 
 sdcc_include_name="SDCC_INCLUDE"
-AC_DEFINE_UNQUOTED(SDCC_INCLUDE_NAME, "${sdcc_include_name}")
+AC_DEFINE_UNQUOTED(SDCC_INCLUDE_NAME, "${sdcc_include_name}", [XXX])
 
 sdcc_lib_name="SDCC_LIB"
-AC_DEFINE_UNQUOTED(SDCC_LIB_NAME, "${sdcc_lib_name}")
+AC_DEFINE_UNQUOTED(SDCC_LIB_NAME, "${sdcc_lib_name}", [XXX])
 
 # Port selection helper
 # ===========================================================================
@@ -681,7 +681,7 @@ AC_DEFUN([AC_DO_ENABLER], [
     OPT_ENABLE_$2=0
   fi
 
-  AC_DEFINE_UNQUOTED(OPT_ENABLE_$2, [$]OPT_ENABLE_$2)
+  AC_DEFINE_UNQUOTED(OPT_ENABLE_$2, [$]OPT_ENABLE_$2, [XXX])
   AC_SUBST(OPT_ENABLE_$2)
 ])
 
@@ -702,7 +702,7 @@ AC_DEFUN([AC_DO_DISABLER], [
     OPT_DISABLE_$2=0
   fi
 
-  AC_DEFINE_UNQUOTED(OPT_DISABLE_$2, [$]OPT_DISABLE_$2)
+  AC_DEFINE_UNQUOTED(OPT_DISABLE_$2, [$]OPT_DISABLE_$2, [XXX])
   AC_SUBST(OPT_DISABLE_$2)
 ])
 
@@ -722,7 +722,7 @@ AC_DEFUN([AC_DO_PORT], [
     OPT_DISABLE_$3=0
   fi
 
-  AC_DEFINE_UNQUOTED(OPT_DISABLE_$3, [$]OPT_DISABLE_$3)
+  AC_DEFINE_UNQUOTED(OPT_DISABLE_$3, [$]OPT_DISABLE_$3, [XXX])
   AC_SUBST(OPT_DISABLE_$3)
 
   echo $2 >>ports.all
@@ -747,7 +747,7 @@ AC_DEFUN([AC_DO_PORT_ENABLER], [
     OPT_DISABLE_$3=1
   fi
 
-  AC_DEFINE_UNQUOTED(OPT_DISABLE_$3, [$]OPT_DISABLE_$3)
+  AC_DEFINE_UNQUOTED(OPT_DISABLE_$3, [$]OPT_DISABLE_$3, [XXX])
   AC_SUBST(OPT_DISABLE_$3)
 
   echo $2 >>ports.all
@@ -765,7 +765,7 @@ AC_DO_PORT(mcs51, mcs51, MCS51, [Excludes the Intel mcs51 port])
 AC_DO_PORT(gbz80, z80,   GBZ80, [Excludes the Gameboy gbz80 port])
 AC_DO_PORT(z80,   z80,   Z80,   [Excludes the z80 port])
 AC_DO_PORT(ds390, ds390, DS390, [Excludes the DS390 port])
-AC_DEFINE_UNQUOTED(OPT_DISABLE_TININative, $OPT_DISABLE_DS390)
+AC_DEFINE_UNQUOTED(OPT_DISABLE_TININative, $OPT_DISABLE_DS390, [XXX])
 AC_DO_PORT(ds400, ds400, DS400, [Excludes the DS400 port])
 AC_DO_PORT(pic,   pic,   PIC,   [Excludes the PIC port])
 AC_DO_PORT(pic16, pic16, PIC16, [Excludes the PIC16 port])

trunk/sdcc/sim/ucsim/configure.in
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 AC_PREREQ(2.60)
-AC_INIT(.version)
+AC_INIT
+AC_CONFIG_SRCDIR(.version)
 AC_CONFIG_HEADER(ddconfig.h:ddconfig_in.h)
 
 AC_PROG_AWK
@@ -21,10 +22,10 @@ AC_SUBST(VERSION)
 AC_SUBST(VERSIONHI)
 AC_SUBST(VERSIONLO)
 AC_SUBST(VERSIONP)
-AC_DEFINE_UNQUOTED(VERSIONSTR, "${VERSION}")
-AC_DEFINE_UNQUOTED(VERSIONHI, $VERSIONHI)
-AC_DEFINE_UNQUOTED(VERSIONLO, $VERSIONLO)
-AC_DEFINE_UNQUOTED(VERSIONP, $VERSIONP)
+AC_DEFINE_UNQUOTED(VERSIONSTR, "${VERSION}", [XXX])
+AC_DEFINE_UNQUOTED(VERSIONHI, $VERSIONHI, [XXX])
+AC_DEFINE_UNQUOTED(VERSIONLO, $VERSIONLO, [XXX])
+AC_DEFINE_UNQUOTED(VERSIONP, $VERSIONP, [XXX])
 
 # Some options
 #==============
@@ -99,7 +100,7 @@ if test $enable_statistic != "no"; then
    enable_statistic="yes"
 fi,
 enable_statistic="no")
-AC_DEFINE_UNQUOTED(STATISTIC, $enable_statistic)
+AC_DEFINE_UNQUOTED(STATISTIC, $enable_statistic, [XXX])
 
 
 # Required programs
@@ -119,7 +120,7 @@ AC_SUBST(LIBTOOL_DEPS)
 
 AC_PROG_LEX
 if test ${LEX} = "flex"; then
-        AC_DEFINE(HAVE_FLEX)
+        AC_DEFINE(HAVE_FLEX, 1, [XXX])
 fi
 AC_PROG_YACC
 AC_CHECK_PROG(BISON_PLUS_PLUS, bison++, bison++, :)
@@ -139,25 +140,25 @@ AC_CHECK_HEADERS(endian.h sys/endian.h machine/endian.h sys/isa_defs.h)
 AC_CHECK_HEADERS(getopt.h unistd.h termios.h)
 
 AC_CHECK_HEADER(curses.h,
-  AC_DEFINE(HAVE_CURSES_H)
+  AC_DEFINE(HAVE_CURSES_H, 1, [XXX])
   curses_h_ok=yes,
   curses_h_ok=no
 )
 
 ucsim_cv_socket="unknown"
 AC_CHECK_HEADER(sys/socket.h,
-  AC_DEFINE(HAVE_SYS_SOCKET_H)
-  AC_DEFINE(SOCKET_AVAIL)
-  AC_DEFINE_UNQUOTED(UCSOCKET_T, int)
+  AC_DEFINE(HAVE_SYS_SOCKET_H, 1, [XXX])
+  AC_DEFINE(SOCKET_AVAIL, 1, [XXX])
+  AC_DEFINE_UNQUOTED(UCSOCKET_T, int, [XXX])
   ucsim_cv_socket="<sys/socket.h>"
 )
 
 WINSOCK_AVAIL=0
 if test $ucsim_cv_socket = unknown; then
   AC_CHECK_HEADER(winsock2.h,
-    AC_DEFINE(HAVE_WINSOCK2_H)
-    AC_DEFINE(SOCKET_AVAIL)
-    AC_DEFINE_UNQUOTED(UCSOCKET_T, SOCKET)
+    AC_DEFINE(HAVE_WINSOCK2_H, 1, [XXX])
+    AC_DEFINE(SOCKET_AVAIL, 1, [XXX])
+    AC_DEFINE_UNQUOTED(UCSOCKET_T, SOCKET, [XXX])
     ucsim_cv_socket="<winsock2.h>"
     WINSOCK_AVAIL=1
   )
@@ -167,10 +168,10 @@ AC_SUBST(WINSOCK_AVAIL)
 if test $ucsim_cv_socket != unknown; then
   AC_EGREP_HEADER(socklen_t,
     $ucsim_cv_socket,
-    AC_DEFINE_UNQUOTED(SOCKLEN_T, socklen_t),
-    AC_DEFINE_UNQUOTED(SOCKLEN_T, uint))
+    AC_DEFINE_UNQUOTED(SOCKLEN_T, socklen_t, [XXX]),
+    AC_DEFINE_UNQUOTED(SOCKLEN_T, uint, [XXX]))
 fi
-AC_DEFINE_UNQUOTED(HEADER_SOCKET, ${ucsim_cv_socket})
+AC_DEFINE_UNQUOTED(HEADER_SOCKET, ${ucsim_cv_socket}, [XXX])
 
 AC_CHECK_HEADER(dlfcn.h)
 AC_HEADER_DIRENT
@@ -202,32 +203,32 @@ yes
 
 ucsim_cv_header_ok=0
 if echo $ucsim_cv_fd|grep time >/dev/null 2>&1; then
-AC_DEFINE(FD_NEED_TIME_H, 1)
+AC_DEFINE(FD_NEED_TIME_H, 1, [XXX])
 ucsim_cv_header_ok=1
 else
-AC_DEFINE(FD_NEED_TIME_H, 0)
+AC_DEFINE(FD_NEED_TIME_H, 0, [XXX])
 fi
 if echo $ucsim_cv_fd|grep types >/dev/null 2>&1; then
-AC_DEFINE(FD_NEED_TYPES_H, 1)
+AC_DEFINE(FD_NEED_TYPES_H, 1, [XXX])
 ucsim_cv_header_ok=1
 else
-AC_DEFINE(FD_NEED_TYPES_H, 0)
+AC_DEFINE(FD_NEED_TYPES_H, 0, [XXX])
 fi
 if echo $ucsim_cv_fd|grep select >/dev/null 2>&1; then
-AC_DEFINE(FD_NEED_SELECT_H, 1)
+AC_DEFINE(FD_NEED_SELECT_H, 1, [XXX])
 ucsim_cv_header_ok=1
 else
-AC_DEFINE(FD_NEED_SELECT_H, 0)
+AC_DEFINE(FD_NEED_SELECT_H, 0, [XXX])
 fi
 if echo $ucsim_cv_fd|grep winsock2 >/dev/null 2>&1; then
-AC_DEFINE(FD_NEED_WINSOCK2_H, 1)
+AC_DEFINE(FD_NEED_WINSOCK2_H, 1, [XXX])
 ucsim_cv_header_ok=1
 else
-AC_DEFINE(FD_NEED_WINSOCK2_H, 0)
+AC_DEFINE(FD_NEED_WINSOCK2_H, 0, [XXX])
 fi
 
-AC_DEFINE_UNQUOTED(FD_HEADER_OK, ${ucsim_cv_header_ok})
-AC_DEFINE_UNQUOTED(HEADER_FD, ${ucsim_cv_fd})
+AC_DEFINE_UNQUOTED(FD_HEADER_OK, ${ucsim_cv_header_ok}, [XXX])
+AC_DEFINE_UNQUOTED(HEADER_FD, ${ucsim_cv_fd}, [XXX])
 
 
 # Checking for functions/libs
@@ -327,7 +328,7 @@ ucsim_cv_scanf_a="no",
 ucsim_cv_scanf_a="unknown")
 ])
 if test "$ucsim_cv_scanf_a" = "yes"; then
-  AC_DEFINE(SCANF_A)
+  AC_DEFINE(SCANF_A, 1, [XXX])
 fi
 
 AC_CACHE_CHECK(whether getcwd is GNUish, ucsim_cv_getcwd,
@@ -344,10 +345,10 @@ ucsim_cv_getcwd="no",
 ucsim_cv_getcwd="unknown")
 ])
 if test "$ucsim_cv_getcwd" = "yes"; then
-  AC_DEFINE(GNU_GETCWD)
+  AC_DEFINE(GNU_GETCWD, 1, [XXX])
 fi
 
-AC_DEFUN(ucsim_ACCEPT_LENGTH_T,
+AC_DEFUN([ucsim_ACCEPT_LENGTH_T],
  [AC_CACHE_CHECK([for type of length pointer parameter of accept],
    ucsim_cv_accept_length_type,
    [ac_save_CPPFLAGS="$CPPFLAGS"
@@ -375,7 +376,7 @@ ucsim_ACCEPT_LENGTH_T
 
 # Check whether the compiler for the current language is SunPRO.
 
-AC_DEFUN(bor_IS_SUNPRO,
+AC_DEFUN([bor_IS_SUNPRO],
 [AC_CACHE_CHECK([whether we are using the SunPRO _AC_LANG compiler],
   [bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro],
   [_AC_COMPILE_IFELSE([AC_LANG_PROGRAM([], [[#if !defined __SUNPRO_C && !defined __SUNPRO_CC
@@ -392,7 +393,7 @@ bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro=$bor_compiler_sunpro
 # the compiler returns an error or in case something is writeen to stderr.
 # For example: bor_VALID_OPT(fPIC)
 
-AC_DEFUN(bor_IS_VALID_OPTION,
+AC_DEFUN([bor_IS_VALID_OPTION],
 [ac_test_[]_AC_LANG_PREFIX[]FLAGS=${[]_AC_LANG_PREFIX[]FLAGS+set}
 bor_save_[]_AC_LANG_PREFIX[]FLAGS=$[]_AC_LANG_PREFIX[]FLAGS
 AC_CACHE_CHECK(whether $_AC_LANG_PREFIX accepts -$1, bor_cv_[]_AC_LANG_ABBREV[]_$1,
@@ -615,9 +616,14 @@ AC_MSG_RESULT($TYPE_WORD)
 AC_MSG_CHECKING(type name for dword)
 TYPE_DWORD=`type_name 4`
 AC_MSG_RESULT($TYPE_DWORD)
-AC_DEFINE_UNQUOTED(TYPE_BYTE, $TYPE_BYTE)
-AC_DEFINE_UNQUOTED(TYPE_WORD, $TYPE_WORD)
-AC_DEFINE_UNQUOTED(TYPE_DWORD, $TYPE_DWORD)
+AC_DEFINE_UNQUOTED(TYPE_BYTE, [$TYPE_BYTE], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_WORD, [$TYPE_WORD], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_DWORD, [$TYPE_DWORD], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UBYTE, [unsigned $TYPE_BYTE], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UWORD, [unsigned $TYPE_WORD], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UDWORD, [unsigned $TYPE_DWORD], [XXX])
+
+AH_BOTTOM([#include "custom.h"])
 
 if echo $TYPE_DWORD|grep short >/dev/null; then
     A="h"
@@ -632,8 +638,8 @@ elif echo $TYPE_WORD|grep long >/dev/null; then M="l"
 else M=""
 fi
 
-AC_DEFINE_UNQUOTED(_A_, "${A}")
-AC_DEFINE_UNQUOTED(_M_, "${M}")
+AC_DEFINE_UNQUOTED(_A_, "${A}", [XXX])
+AC_DEFINE_UNQUOTED(_M_, "${M}", [XXX])
 
 # Generating output files
 # ===========================================================================

trunk/sdcc/debugger/mcs51/configure.in
@@ -46,7 +46,7 @@ fi
 
 # Check whether the compiler for the current language is SunPRO.
 
-AC_DEFUN(bor_IS_SUNPRO,
+AC_DEFUN([bor_IS_SUNPRO],
 [AC_CACHE_CHECK([whether we are using the SunPRO _AC_LANG compiler],
   [bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro],
   [_AC_COMPILE_IFELSE([AC_LANG_PROGRAM([], [[#if !defined __SUNPRO_C && !defined __SUNPRO_CC
@@ -63,7 +63,7 @@ bor_cv_[]_AC_LANG_ABBREV[]_compiler_sunpro=$bor_compiler_sunpro
 # the compiler returns an error or in case something is writeen to stderr.
 # For example: bor_VALID_OPT(fPIC)
 
-AC_DEFUN(bor_IS_VALID_OPTION,
+AC_DEFUN([bor_IS_VALID_OPTION],
 [ac_test_[]_AC_LANG_PREFIX[]FLAGS=${[]_AC_LANG_PREFIX[]FLAGS+set}
 bor_save_[]_AC_LANG_PREFIX[]FLAGS=$[]_AC_LANG_PREFIX[]FLAGS
 AC_CACHE_CHECK(whether $_AC_LANG_PREFIX accepts -$1, bor_cv_[]_AC_LANG_ABBREV[]_$1,
@@ -144,7 +144,7 @@ dnl adl_NORMALIZE_DEFINE_UNQUOTED(var, DEFINE, REFERENCE_STRING)
 AC_DEFUN([adl_NORMALIZE_DEFINE_UNQUOTED], [
   ac_ndu=[$]$1
   adl_NORMALIZE_PATH([ac_ndu], [$]$3)
-  AC_DEFINE_UNQUOTED($2, "${ac_ndu}")
+  AC_DEFINE_UNQUOTED($2, "${ac_ndu}", [XXX])
 ])
 
 
@@ -224,7 +224,7 @@ AC_SUBST(EXEEXT)
 # sdccconf.h
 ############
 
-AC_DEFINE_UNQUOTED(DIR_SEPARATOR_STRING, "${sdccconf_h_dir_separator}")
+AC_DEFINE_UNQUOTED(DIR_SEPARATOR_STRING, "${sdccconf_h_dir_separator}", [XXX])
 
 # datadir:
 # default: "${prefix}/share"
@@ -235,7 +235,7 @@ adl_NORMALIZE_DEFINE_UNQUOTED(expanded_datadir, DATADIR, sdccconf_h_dir_separato
 norm_lib_dir_suffix=${lib_dir_suffix}
 adl_NORMALIZE_PATH([norm_lib_dir_suffix], [$sdccconf_h_dir_separator])
 AC_DEFINE_UNQUOTED(LIB_DIR_SUFFIX,
-                   DIR_SEPARATOR_STRING "${norm_lib_dir_suffix}")
+                   DIR_SEPARATOR_STRING "${norm_lib_dir_suffix}", [XXX])
 
 
 # Generating output files

trunk/sdcc/device/lib/pic/configure.in
@@ -17,7 +17,7 @@
 AC_PREREQ(2.60)
 AC_INIT
 
-AC_DEFUN(AC_CONFIG_SRCDIR)
+AC_DEFUN([AC_CONFIG_SRCDIR])
 #AC_DEFUN(AC_CONFIG_FILES)
 
 # Checks for programs.

trunk/sdcc/support/packihx/configure.in
@@ -41,7 +41,12 @@ AC_MSG_RESULT($TYPE_BYTE)
 AC_MSG_CHECKING(type name for word)
 TYPE_WORD=`type_name 2`
 AC_MSG_RESULT($TYPE_WORD)
-AC_DEFINE_UNQUOTED(TYPE_BYTE, $TYPE_BYTE)
-AC_DEFINE_UNQUOTED(TYPE_WORD, $TYPE_WORD)
+AC_DEFINE_UNQUOTED(TYPE_BYTE, [$TYPE_BYTE], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_WORD, [$TYPE_WORD], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UBYTE, [unsigned $TYPE_BYTE], [XXX])
+AC_DEFINE_UNQUOTED(TYPE_UWORD, [unsigned $TYPE_WORD], [XXX])
+
+AH_BOTTOM([typedef TYPE_UBYTE Uint8;
+typedef TYPE_UWORD Uint16;])
 
 AC_OUTPUT(Makefile)

trunk/sdcc/device/lib/pic16/configure.ac
@@ -3,7 +3,7 @@
 
 AC_PREREQ(2.61)
 AC_INIT([pic16lib], [0.3], [tecodev AT users sourceforge net])
-AM_INIT_AUTOMAKE
+AM_INIT_AUTOMAKE([foreign])
 AC_CONFIG_SRCDIR([debug/gstack/gstack.c])
 AC_CONFIG_HEADER([config.h])
 

trunk/sdcc/sim/ucsim/custom.h
@@ -1,5 +1,5 @@
 /*
- * Simulator of microcontrollers (ddconfig.h)
+ * Simulator of microcontrollers (custom.h)
  *
  * Copyright (C) 1999,99 Drotos Daniel, Talker Bt.
  *
@@ -25,85 +25,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA. */
 /*@1@*/
 
-#ifndef DDCONFIG_HEADER
-#define DDCONFIG_HEADER
+#ifndef CUSTOM_HEADER
+#define CUSTOM_HEADER
 
-#define DD_TRUE  1
-#define DD_FALSE 0
-#define NIL     0
-
-#undef STDC_HEADERS
-#undef HAVE_GETOPT_H
-#undef HAVE_UNISTD_H
-#undef HAVE_DIRENT_H
-#undef HAVE_SYS_NDIR_H
-#undef HAVE_SYS_DIR_H
-#undef HAVE_NDIR_H
-#undef HAVE_SYS_SOCKET_H
-#undef HAVE_WINSOCK2_H
-#undef HAVE_DLFCN_H
-#undef HAVE_CURSES_H
-#undef HAVE_TERMIOS_H
-#undef HAVE_ENDIAN_H
-#undef HAVE_SYS_ENDIAN_H
-#undef HAVE_MACHINE_ENDIAN_H
-#undef HAVE_SYS_ISA_DEFS_H
-
-#undef SOCKET_AVAIL
-#undef SOCKLEN_T
-#undef UCSOCKET_T
-#undef HEADER_SOCKET
-
-#undef FD_NEED_TIME_H
-#undef FD_NEED_TYPES_H
-#undef FD_NEED_SELECT_H
-#undef FD_NEED_WINSOCK2_H
-#undef HEADER_FD
-#undef FD_HEADER_OK
-
-#undef SCANF_A
-#undef GNU_GETCWD
-
-#undef HAVE_STRLEN
-#undef HAVE_STRCPY
-#undef HAVE_STRCAT
-#undef HAVE_STRSTR
-#undef HAVE_STRCMP
-#undef HAVE_STRERROR
-#undef HAVE_STRTOK
-#undef HAVE_STRDUP
-#undef HAVE_STRCHR
-
-#undef HAVE_MEMCPY
-
-#undef HAVE_VPRINTF
-#undef HAVE_DOPRNT
-#undef HAVE_VSNPRINTF
-#undef HAVE__VSNPRINTF
-#undef HAVE_VASPRINTF
-
-#undef HAVE_GETLINE
-#undef HAVE_GETDELIM
-#undef HAVE_FGETS
-
-#undef HAVE_YYLEX
-#undef HAVE_FLEX
-#undef YYTEXT_POINTER
-
-#undef RETSIGTYPE
-#undef SIZEOF_CHAR
-#undef SIZEOF_SHORT
-#undef SIZEOF_INT
-#undef SIZEOF_LONG
-#undef SIZEOF_LONG_LONG
-#undef TYPE_BYTE
-#undef TYPE_WORD
-#undef TYPE_DWORD
-#define TYPE_UBYTE unsigned TYPE_BYTE
-#define TYPE_UWORD unsigned TYPE_WORD
-#define TYPE_UDWORD unsigned TYPE_DWORD
-#undef _M_
-#undef _A_
+#define DD_TRUE     1
+#define DD_FALSE    0
+#define NIL         0
 
 /*
  * find out the endianess of host machine
@@ -143,15 +70,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 # endif
 #endif
 
-#undef VERSIONSTR
-#undef VERSIONHI
-#undef VERSIONLO
-#undef VERSIONP
-
-#undef ACCEPT_SOCKLEN_T
-
-#undef STATISTIC
-
 #endif
 
-/* End of ddconfig.h */
+/* End of custom.h */

commit 00a0d36b768f8017954eee28b594fa3e88eff24e

* configure.in, custom_in.h:   revive support for big-endian machines, broken since r5954 * sim/ucsim/configure.in, sim/ucsim/custom.h, sim/ucsim/custom_in.h:   have configure create custom.h next to ddconfig.h in the build tree   so that CPP will find it
git-svn-id: svn://svn.code.sf.net/p/sdcc/code@5970 4a8a32a2-be11-0410-ad9d-d568d2c75423

trunk/sdcc/configure.in
@@ -3,7 +3,7 @@
 AC_PREREQ(2.60)
 AC_INIT(.version)
 AC_CONFIG_SRCDIR([Makefile.in])
-AC_CONFIG_HEADER([sdccconf.h:sdccconf_in.h sdas/linksrc/asxxxx_config.h])
+AC_CONFIG_HEADER([sdccconf.h:sdccconf_in.h custom.h:custom_in.h sdas/linksrc/asxxxx_config.h])
 
 AC_PROG_AWK
 
@@ -68,7 +68,7 @@ SDCC_REQUIRE_PROG($LEX, flex)
 
 AC_LANG_C
 
-AC_CHECK_HEADERS(endian.h sys/endian.h machine/endian.h)
+AC_CHECK_HEADERS(endian.h sys/endian.h machine/endian.h sys/isa_defs.h)
 
 AC_ARG_WITH([ccache],
     AC_HELP_STRING([--without-ccache], [do not use ccache even if available]),
@@ -665,6 +665,12 @@ AC_DEFINE_UNQUOTED(SDCC_INCLUDE_NAME, "${sdcc_include_name}", [XXX])
 
 sdcc_lib_name="SDCC_LIB"
 AC_DEFINE_UNQUOTED(SDCC_LIB_NAME, "${sdcc_lib_name}", [XXX])
+
+# add include guard and custom stuff to sdccconf.h
+AH_TOP([#ifndef SDCCCONF_HEADER
+#define SDCCCONF_HEADER])
+AH_BOTTOM([#include "custom.h"
+#endif /* SDCCCONF_HEADER */])
 
 # Port selection helper
 # ===========================================================================

trunk/sdcc/custom_in.h
@@ -1,36 +1,4 @@
-/*
- * Simulator of microcontrollers (custom.h)
- *
- * Copyright (C) 1999,99 Drotos Daniel, Talker Bt.
- *
- * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
- *
- */
-
-/* This file is part of microcontroller simulator: ucsim.
-
-UCSIM is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-UCSIM is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with UCSIM; see the file COPYING.  If not, write to the Free
-Software Foundation, 59 Temple Place - Suite 330, Boston, MA
-02111-1307, USA. */
-/*@1@*/
-
-#ifndef CUSTOM_HEADER
-#define CUSTOM_HEADER
-
-#define DD_TRUE     1
-#define DD_FALSE    0
-#define NIL         0
+/* custom.h -- manual additions to #include into sdccconf.h */
 
 /*
  * find out the endianess of host machine
@@ -62,14 +30,9 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #   endif
 # else
 /* 3) trust the configure; this actually doesn't work for unified Mac OS X binaries :-( */
-#   undef BUILD_WORDS_BIGENDIAN
 #   if (defined BUILD_WORDS_BIGENDIAN && BUILD_WORDS_BIGENDIAN)
 #     define WORDS_BIGENDIAN  1
 #   endif
 /* 4) assume that host is a little endian machine */
 # endif
 #endif
-
-#endif
-
-/* End of custom.h */

trunk/sdcc/sim/ucsim/configure.in
@@ -3,7 +3,7 @@
 AC_PREREQ(2.60)
 AC_INIT
 AC_CONFIG_SRCDIR(.version)
-AC_CONFIG_HEADER(ddconfig.h:ddconfig_in.h)
+AC_CONFIG_HEADER(ddconfig.h:ddconfig_in.h custom.h:custom_in.h)
 
 AC_PROG_AWK
 
@@ -623,7 +623,10 @@ AC_DEFINE_UNQUOTED(TYPE_UBYTE, [unsigned $TYPE_BYTE], [XXX])
 AC_DEFINE_UNQUOTED(TYPE_UWORD, [unsigned $TYPE_WORD], [XXX])
 AC_DEFINE_UNQUOTED(TYPE_UDWORD, [unsigned $TYPE_DWORD], [XXX])
 
-AH_BOTTOM([#include "custom.h"])
+AH_TOP([#ifndef DDCONFIG_HEADER
+#define DDCONFIG_HEADER])
+AH_BOTTOM([#include "custom.h"
+#endif /* DDCONFIG_HEADER */])
 
 if echo $TYPE_DWORD|grep short >/dev/null; then
     A="h"

trunk/sdcc/sim/ucsim/custom_in.h
@@ -1,0 +1,71 @@
+/*
+ * Simulator of microcontrollers (custom.h)
+ *
+ * This file contains manual additions to #include into ddconfig.h
+ *
+ * Copyright (C) 1999,99 Drotos Daniel, Talker Bt.
+ *
+ * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
+ *
+ */
+
+/* This file is part of microcontroller simulator: ucsim.
+
+UCSIM is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+UCSIM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UCSIM; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA. */
+/*@1@*/
+
+#define DD_TRUE     1
+#define DD_FALSE    0
+#define NIL         0
+
+/*
+ * find out the endianess of host machine
+ */
+#if defined __APPLE__ && (defined __BIG_ENDIAN__ || defined _BIG_ENDIAN)
+/* 1) trust the compiler
+ * in order to be able to make Mac OS X unified binaries */
+# define WORDS_BIGENDIAN 1
+#elif defined __APPLE__ && (defined __LITTLE_ENDIAN__ || defined _LITTLE_ENDIAN)
+/* do nothing */
+#elif defined __sun && defined HAVE_SYS_ISA_DEFS_H
+/* Solaris defines endianness in <sys/isa_defs.h> */
+# include <sys/isa_defs.h>
+# ifdef _BIG_ENDIAN
+#   define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifdef HAVE_ENDIAN_H
+#   include <endian.h>
+# elif defined HAVE_SYS_ENDIAN_H
+#   include <sys/endian.h>
+# elif defined HAVE_MACHINE_ENDIAN_H
+#   include <machine/endian.h>
+# endif
+# if (defined BYTE_ORDER && defined BIG_ENDIAN && defined LITTLE_ENDIAN && BYTE_ORDER && BIG_ENDIAN && LITTLE_ENDIAN)
+/* 2) trust the header files */
+#   if BYTE_ORDER == BIG_ENDIAN
+#     define WORDS_BIGENDIAN 1
+#   endif
+# else
+/* 3) trust the configure; this actually doesn't work for unified Mac OS X binaries :-( */
+#   if (defined BUILD_WORDS_BIGENDIAN && BUILD_WORDS_BIGENDIAN)
+#     define WORDS_BIGENDIAN  1
+#   endif
+/* 4) assume that host is a little endian machine */
+# endif
+#endif
+
+/* End of custom.h */